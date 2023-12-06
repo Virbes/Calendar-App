705aac92871101384392d0a88fc212e0f420bd43
Virbes <francisco.virbes.457@gmail.com>
2023-12-06 13:24:24 -0600

    additionals

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+export const PrivateRoute = ({ isAuthenticated }) => {
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
